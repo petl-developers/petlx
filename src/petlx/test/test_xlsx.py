0e320850f6b26b74647ef20f54030bbfcc357790
Alistair Miles <alimanfoo@googlemail.com>
2014-02-17 22:11:23 +0000

    fix tests for unicode and datetime in fixture

--- src/petlx/test/test_xlsx.py
+++ src/petlx/test/test_xlsx.py
@@ -1,6 +1,10 @@
+# -*- coding: utf-8 -*-
+
+
 from petlx.xlsx import fromxlsx
 import petl.fluent as etl
 from petl.testutils import ieq
+from datetime import datetime
 
 
 def test_fromxlsx():
@@ -8,16 +12,18 @@ def test_fromxlsx():
     expect = (('foo', 'bar'),
               ('A', 1),
               ('B', 2),
-              ('C', 2))
+              ('C', 2),
+              (u'é', datetime(2012, 1, 1)))
     ieq(expect, tbl)
 
 
 def test_integration():
-    tbl = etl.fromxlsx('fixture/test.xlsx', 'Sheet1').convert('bar', int)
+    tbl = etl.fromxlsx('fixture/test.xlsx', 'Sheet1')
     expect = (('foo', 'bar'),
               ('A', 1),
               ('B', 2),
-              ('C', 2))
+              ('C', 2),
+              (u'é', datetime(2012, 1, 1)))
     ieq(expect, tbl)
 
 
