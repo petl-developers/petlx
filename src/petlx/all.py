f03e3882d97a0223cc54a3c3766f21aa4be424cf
Alistair Miles <alimanfoo@googlemail.com>
2014-06-30 16:32:03 +0100

    closes #60

---
+++ src/petlx/all.py
@@ -0,0 +1,21 @@
+__author__ = 'Alistair Miles <alimanfoo@googlemail.com>'
+
+
+"""
+Meta-module to import all petlx modules and thereby cause them to be integrated
+with petl.fluent and petl.interactive.
+"""
+
+
+import petlx.array
+import petlx.dataframe
+import petlx.gff3
+import petlx.hdf5
+import petlx.index
+import petlx.interval
+import petlx.ipython
+import petlx.sql
+import petlx.tabix
+import petlx.vcf
+import petlx.xls
+import petlx.xlsx
