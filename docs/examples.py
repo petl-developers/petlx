5d33a8c7ab88e1edaa18b6ac8ba407eff4e7fb77
Alistair Miles <alimanfoo@googlemail.com>
2015-01-15 15:54:45 +0000

    migrate various to petl

--- docs/examples.py
+++ docs/examples.py
@@ -5,129 +5,6 @@ Examples.
 
 
 
-table = (('type', 'start', 'stop', 'value'),
-         ('apple', 1, 4, 'foo'),
-         ('apple', 3, 7, 'bar'),
-         ('orange', 4, 9, 'baz'))
-
-from petl import look
-from petlx.interval import facetintervallookup
-look(table)
-lkp = facetintervallookup(table, facet='type', startfield='start', stopfield='stop')
-lkp['apple'][1:2]
-lkp['apple'][2:4]
-lkp['apple'][2:5]
-lkp['orange'][2:5]
-lkp['orange'][9:14]
-lkp['orange'][19:140]
-lkp['apple'][1]
-lkp['apple'][2]
-lkp['apple'][4]
-lkp['apple'][5]
-lkp['orange'][5]
-
-table = (('type', 'start', 'stop', 'value'),
-         ('apple', 1, 4, 'foo'),
-         ('apple', 3, 7, 'bar'),
-         ('orange', 4, 9, 'baz'))
-
-from petl import look
-from petlx.interval import facetintervallookupone
-look(table)
-lkp = facetintervallookupone(table, key='type', startfield='start', stopfield='stop', valuespec='value')
-lkp['apple'][1:2]
-lkp['apple'][2:4]
-lkp['apple'][2:5]
-lkp['apple'][4:5]
-lkp['orange'][4:5]
-lkp['apple'][5:7]
-lkp['orange'][5:7]
-lkp['apple'][8:9]
-lkp['orange'][8:9]
-lkp['orange'][9:14]
-lkp['orange'][19:140]
-lkp['apple'][1]
-lkp['apple'][2]
-lkp['apple'][4]
-lkp['apple'][5]
-lkp['orange'][5]
-lkp['apple'][8]
-lkp['orange'][8]
-    
-    
-table = (('type', 'start', 'stop', 'value'),
-         ('apple', 1, 4, 'foo'),
-         ('apple', 3, 7, 'bar'),
-         ('orange', 4, 9, 'baz'))
-
-from petl import look
-from petlx.interval import facetintervalrecordlookup
-look(table)
-lkp = facetintervalrecordlookup(table, key='type', startfield='start', stopfield='stop')
-lkp['apple'][1:2]
-lkp['apple'][2:4]
-lkp['apple'][2:5]
-lkp['orange'][2:5]
-lkp['orange'][9:14]
-lkp['orange'][19:140]
-lkp['apple'][1]
-lkp['apple'][2]
-lkp['apple'][4]
-lkp['apple'][5]
-lkp['orange'][5]
-
-table = (('type', 'start', 'stop', 'value'),
-         ('apple', 1, 4, 'foo'),
-         ('apple', 3, 7, 'bar'),
-         ('orange', 4, 9, 'baz'))
-
-from petl import look
-from petlx.interval import facetintervalrecordlookupone
-look(table)
-lkp = facetintervalrecordlookupone(table, key='type', startfield='start', stopfield='stop')
-lkp['apple'][1:2]
-lkp['apple'][2:4]
-lkp['apple'][2:5]
-lkp['apple'][4:5]
-lkp['orange'][4:5]
-lkp['apple'][5:7]
-lkp['orange'][5:7]
-lkp['apple'][8:9]
-lkp['orange'][8:9]
-lkp['orange'][9:14]
-lkp['orange'][19:140]
-lkp['apple'][1]
-lkp['apple'][2]
-lkp['apple'][4]
-lkp['apple'][5]
-lkp['orange'][5]
-lkp['apple'][8]
-lkp['orange'][8]
-
-
-# intervaljoin
-left = (('begin', 'end', 'quux'),
-        (1, 2, 'a'),
-        (2, 4, 'b'),
-        (2, 5, 'c'),
-        (9, 14, 'd'),
-        (9, 140, 'e'),
-        (1, 1, 'f'),
-        (2, 2, 'g'),
-        (4, 4, 'h'),
-        (5, 5, 'i'),
-        (1, 8, 'j'))
-right = (('start', 'stop', 'value'),
-         (1, 4, 'foo'),
-         (3, 7, 'bar'),
-         (4, 9, 'baz'))
-
-from petl import look
-from petlx.interval import intervaljoin
-look(left)
-look(right)
-result = intervaljoin(left, right, lstart='begin', lstop='end', rstart='start', rstop='stop')
-look(result) 
 
 
 # intervaljoin with facet
@@ -139,11 +16,7 @@ left = (('fruit', 'begin', 'end'),
         ('orange', 2, 5),
         ('orange', 9, 14),
         ('orange', 19, 140),
-        ('apple', 1, 1),
-        ('apple', 2, 2),
-        ('apple', 4, 4),
-        ('apple', 5, 5),
-        ('orange', 5, 5))
+        ('apple', 1, 1))
 right = (('type', 'start', 'stop', 'value'),
          ('apple', 1, 4, 'foo'),
          ('apple', 3, 7, 'bar'),
--- docs/index.rst
+++ docs/index.rst
@@ -3,8 +3,10 @@
 petlx - Optional extensions to the petl package
 ===============================================
 
-:mod:`petlx` is a collection of extensions to `petl <http://petl.readthedocs.org/>`_, 
-a Python package for extracting, transforming and loading tables of data.
+:mod:`petlx` is a collection of domain-specific and/or experimental
+extensions to `petl <http://petl.readthedocs.org/>`_, a general
+purpose Python package for extracting, transforming and loading tables
+of data.
 
 - Documentation: http://petlx.readthedocs.org/
 - Source Code: https://github.com/alimanfoo/petlx
@@ -21,35 +23,57 @@ python-etl@googlegroups.com or `raise an issue on GitHub
 For an overview of all functions in the package, see the
 :ref:`genindex`.
 
+
+.. _installation:
+
 Installation
 ------------
 
-This module is available from the `Python Package Index
-<http://pypi.python.org/pypi/petlx>`_. On Linux distributions you
-should be able to do ``easy_install petlx`` or ``pip install
-petlx``. On Windows or Mac you can download manually, extract and run
-``python setup.py install``.
+This package is available from the `Python Package Index
+<http://pypi.python.org/pypi/petlx>`_. If you have `pip
+<https://pip.pypa.io/>`_ you should be able to do::
 
-Note that each submodule within the :mod:`petlx` package has
-dependencies on one or more third party modules which will need to be
-installed separately. 
+    $ pip install petlx
+
+You can also download manually, extract and run ``python setup.py
+install``.
+
+To verify the installation, the test suite can be run with `nose
+<https://nose.readthedocs.org/>`_, e.g.::
+
+    $ pip install nose
+    $ nosetests -v petlx
+
+
+.. _dependencies:
+
+Dependencies
+------------
+
+This package has no installation requirements other than the Python
+core modules.
+
+Some of the functions in this package require installation of third party
+packages. This is indicated in the relevant parts of the documentation.
+
+
+.. _modules:
 
 Modules
 -------
 
 .. note::
 
-   The modules :mod:`petlx.ipython`, :mod:`petlx.array`, :mod:`petlx.dataframe`,
-   :mod:`petlx.xls`, :mod:`petlx.xlsx`, :mod:`petlx.hdf5` have all been
-   migrated to the main :mod:`petl` package. See the
-   `petl docs <http://petl.readthedocs.org/>`_ for more information.
+   The modules :mod:`petlx.ipython`, :mod:`petlx.array`,
+   :mod:`petlx.dataframe`, :mod:`petlx.xls`, :mod:`petlx.xlsx`,
+   :mod:`petlx.hdf5`, :mod:`petlx.sql`, :mod:`petlx.interval` have all been
+   migrated to the main :mod:`petl` package. See the `petl docs
+   <http://petl.readthedocs.org/>`_ for more information.
 
 .. toctree::
    :maxdepth: 2
 
-   sql
    whoosh
-   interval
    tabix
    gff3
    vcf
